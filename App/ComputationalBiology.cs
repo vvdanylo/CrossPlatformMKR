35e7b396c974c51a0126a8c103dcf12434d748e7
vvdanylo <hex.ageha6167656861@gmail.com>
2024-11-13 09:51:35 +0200

    app core logic

---
+++ App/ComputationalBiology.cs
@@ -0,0 +1,37 @@
+﻿namespace App;
+
+public class ComputationalBiology
+{
+    private static readonly string Yes = "YES";
+    private static readonly string No = "NO";
+    public static string Solve(string sequence, string subsequence)
+    {
+        if (string.IsNullOrEmpty(subsequence) || string.IsNullOrEmpty(sequence))
+        {
+            throw new ArgumentException("The input string cannot be null or empty");
+        }
+        if (!(IsGeneticSequence(subsequence) && IsGeneticSequence(sequence)))
+        {
+            throw new ArgumentException("The input string is not a genetic sequence");
+        }
+        if (subsequence.Length < sequence.Length)
+        {
+            throw new ArgumentOutOfRangeException(nameof(subsequence), "Subsequence can't be longer than sequence");
+        }
+
+        var j = 0;
+        for (var i = 0; i < subsequence.Length; i++)
+        {
+            if (char.ToUpper(sequence[j]) == char.ToUpper(subsequence[i])) j++;
+            if (j == sequence.Length) return Yes;
+        }
+        return No;
+    }
+
+    private static bool IsGeneticSequence(string sequence)
+    {
+        return sequence.All(c => c == 'A' || c == 'C' || c == 'G' || c == 'T');
+    }
+
+}
+
---
+++ App/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace App;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string sequence = String.Empty;
+        string subsequence = String.Empty;
+        try
+        {
+            (sequence, subsequence) = FileDataHandler.ReadDnaSequencesFromFile();
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An exeption occured while reading file: {ex.Message}");
+            return;
+        }
+
+        var result = String.Empty;
+        try
+        {
+            result = ComputationalBiology.Solve(sequence, subsequence);
+            Console.WriteLine($"The result is (Does the genetic sequence {sequence} contain given subsequence {subsequence}?): {result}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An exeption occured while solving the problem: {ex.Message}");
+            return;
+        }
+
+        try
+        {
+            FileDataHandler.WriteResultToFile(result);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An exeption occured while writing results to file: {ex.Message}");
+            return;
+        }
+
+    }
+}
